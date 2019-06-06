63a9d34b2bb5d1f1467b967885a8f613b3457dae
jonathan nshuti <nshutijonathan130@gmail.com>
2019-06-06 16:18:50 +0200

    [ft2-database-set-up-166413022]added script to populate database

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,13 +1,33 @@
-import pg from 'pg';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
+let pool = {};
+if (process.env.NODE_ENV === 'development') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+  });
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL
-});
+  pool.on('connect', () => {
+    console.log('connected to database');
+  });
+}
+if (process.env.NODE_ENV === 'testing') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_TESTING
+  });
 
-pool.on('connect', () => {
-  console.log('connected to database');
-});
+  pool.on('connect', () => {
+    console.log('connected to database');
+  });
+}
+if (process.env.NODE_ENV === 'production') {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_HEROKU
+  });
+
+  pool.on('connect', () => {
+    console.log('connected to database');
+  });
+}
 export default pool;
--- server/server.js
+++ server/server.js
@@ -5,14 +5,24 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import router from './routes/routes';
+import pool from './database/connect';
 // creating app instance
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 dotenv.config();
+if (process.env.NODE_ENV === 'development') {
+  console.log(`${process.env.DATABASE_URL} is enabled`);
+}
+if (process.env.NODE_ENV === 'testing') {
+  console.log(`${process.env.DATABASE_TESTING} is enabled`);
+}
+if (process.env.NODE_ENV === 'production') {
+  console.log(`${process.env.DATABASE_HEROKU} is enabled`);
+}
 
-console.log(`${process.env.NODE_ENV} is enabled`);
+console.log(`${app.get('env')}`);
 // home route
 app.get('/', (req, res, next) => {
   return res.status(200).send({
