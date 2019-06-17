94eca42c7c215613bb79edad5496cd8c43e6fd99
nshuti jonathan <nshutijonathan130@gmail.com>
2019-06-18 01:39:03 +0200

    Chore  Build Out all User endpoint tests (#15)
    * [ft2-database-set-up-166413022]initialized .env.
    
    * [ft2-database-set-up-166413022]initialized .gitignore
    
    * [ft2-database-set-up-166413022]initialized package.json.
    
    * [ft2-database-set-up-166413022]installed required modules.
    
    * [ft2-database-set-up-166413022]connected topostgress database
    
    * [ft2-database-set-up-166413022]created auth middleware.
    
    * [ft2-database-set-up-166413022]hashed users password
    
    * [ft2-database-set-up-166413022]added user endpoints.
    
    * [ft2-database-set-up-166413022]initialized user validations.
    
    * [ft2-database-set-up-166413022]validated users.
    
    * [ft2-database-set-up-166413022]configured node environment.
    
    * [ft2-database-set-up-166413022]added script to populate database
    
    * [ft2-database-set-up-166413022]added  user tests.
    
    * [ft2-database-set-up-166413022]added user sign up tests.
    
    * [ft2-database-set-up-166413022]tested admin signup endpoints.
    
    * [ft2-database-set-up-166413022]gitignored dot env.
    
    * [ft2-database-set-up-166413022]gitignored dot env.
    
    * [ft2-database-set-up-166413022].gitignore is now working.
    
    * [ft2-database-set-up-166413022].gitignore is now working.
    
    * [ft3-seller-create-car-sale-ad-166546064]ignored .env
    
    * [ft3-seller-create-car-sale-ad-166546064]initialised node modules.
    
    * [ft3-seller-create-car-sale-ad-166546064]added car advert file.
    
    * [ft3-seller-create-car-sale-ad-166546064]added create car ad endpoint.
    
    * [ft3-seller-create-car-sale-ad-166546064]added cars sales advert validation.
    
    * [ft3-seller-create-car-sale-ad-166546064]added seller role based authorization.
    
    * [ft3-seller-create-car-sale-ad-166546064]added get all car advert endpoint.
    
    * [ft3-seller-create-car-sale-ad-166546064]added user create car sale endpoint.
    
    * [ft3-seller-create-car-sale-ad-166546064]added npm script to run test.
    
    * [ft3-seller-create-car-sale-ad-166546064]added user change pswd endpoint.
    
    * [ft3-seller-create-car-sale-ad-166546064]adde cross env script.
    
    * [ft3-seller-create-car-sale-ad-166546064]initialized nodemailer module
    
    * [ft3-seller-create-car-sale-ad-166546064]unprotected get all car advert.
    
    * [166718882]initlialized purchase orders file
    
    * [ft3-create-purchasing-order-166718882]added orders tables.
    
    * [ft3-create-purchasing-order-166718882]added create purchasing order logic
    
    * [ft3-create-purchasing-order-166718882]added create purchasing order validations.
    
    * [ft3-patch-purchasing-order-166721202]added on cascade query.
    
    * [ft3-patch-purchasing-order-166721202]added get all orders endpoint.
    
    * [ft3-patch-purchasing-order-166721202]added patch order price logic.
    
    * [ft3-mark-car-ad-as-sold-166722088]added seller mark car ad as sold endpoint.
    
    * [user-view-specific-car-166722520]added get specif car endpoint.
    
    * [user-view-specific-car-166722520]added all get endpoints remained.
    
    * [user-view-specific-car-166722520]added maintanability badge.
    
    * [user-view-specific-car-166722520]added coveralls.
    
    * [user-view-specific-car-166722520]added badges.
    
    * [user-view-specific-car-166722520]added travis.yml
    
    * []updated readme file& package.json.
    
    * []added code climate.yml
    
    * [user-view-specific-car-166722520]modified code climate.yml
    
    * [ch3-testing-heroku]tested endpoint get all users
    
    * [ch3-testing-heroku]added signin user test.
    
    * [ch3-testing-heroku]tested admin sign up endpoint.
    
    * [ch3-testing-heroku]tested all users endpoints.

--- server/tests/users.js
+++ server/tests/users.js
@@ -7,6 +7,8 @@ import server from '../server';
 dotenv.config();
 chai.use(chaiHttp);
 chai.should();
+const token = process.env.jwtPrivateKey;
+const fakeToken = process.env.INVALID_TOKEN;
 describe('get welcome message', () => {
   it('should return welcome message', (done) => {
     chai.request(server).get('/').end((err, res) => {
@@ -29,6 +31,7 @@ describe('Users', () => {
       user_type: 'buyer',
       is_admin: 'false'
     }).end((err, res) => {
+      console.log(res.body);
       res.body.should.be.an('object');
       res.body.should.have.property('status').eql(201);
       res.body.should.have.property('message').eql('User successfully created');
@@ -47,6 +50,39 @@ describe('Users', () => {
   it('should not register new user ', (done) => {
     chai.request(server).post('/api/v2/auth/signup').send({
       email: 'alice@gmail.com',
+      first_name: 'nshuti',
+      last_name: 'jonathan',
+      password: 'chris@gmail.com',
+      address: 'kigali',
+      user_type: 'buyer',
+      is_admin: 'false'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(409);
+      res.body.should.have.property('message').eql('User with that Email already exists');
+      done();
+    });
+  });
+  it('should not register new user ', (done) => {
+    chai.request(server).post('/api/v2/auth/signup').send({
+      email: '',
+      first_name: 'nshuti',
+      last_name: 'jonathan',
+      password: 'chris@gmail.com',
+      address: 'kigali',
+      user_type: 'buyer',
+      is_admin: 'false'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('message').eql('email is required');
+      done();
+    });
+  });
+  it('should not register new user ', (done) => {
+    chai.request(server).post('/api/v2/auth/signup').send({
+      email: 'alice@gmail.com',
       first_name: '',
       last_name: 'jonathan',
       password: 'chris@gmail.com',
@@ -54,19 +90,235 @@ describe('Users', () => {
       user_type: 'buyer',
       is_admin: 'false'
     }).end((err, res) => {
+      console.log(res.body);
       res.body.should.be.an('object');
+      res.body.should.have.property('message').eql('first_name is required');
+      done();
+    });
+  });
+  it('should not register new user ', (done) => {
+    chai.request(server).post('/api/v2/auth/signup').send({
+      email: 'alice@gmail.com',
+      first_name: 'nshuti',
+      last_name: '',
+      password: 'chris@gmail.com',
+      address: 'kigali',
+      user_type: 'buyer',
+      is_admin: 'false'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('message').eql('last_name is required');
+      done();
+    });
+  });
+  it('should not register new user ', (done) => {
+    chai.request(server).post('/api/v2/auth/signup').send({
+      email: 'alice@gmail.com',
+      first_name: 'nshuti',
+      last_name: 'jonathan',
+      password: '',
+      address: 'kigali',
+      user_type: 'buyer',
+      is_admin: 'false'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('message').eql('password is required');
+      done();
+    });
+  });
+  it('should not register new user ', (done) => {
+    chai.request(server).post('/api/v2/auth/signup').send({
+      email: 'alice@gmail.com',
+      first_name: 'nshuti',
+      last_name: 'jonathan',
+      password: 'alice@gmail.com@@##',
+      address: '',
+      user_type: 'buyer',
+      is_admin: 'false'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('message').eql('address is required');
+      done();
+    });
+  });
+  it('should not register new user ', (done) => {
+    chai.request(server).post('/api/v2/auth/signup').send({
+      email: 'alice@gmail',
+      first_name: 'nshuti',
+      last_name: 'jonathan',
+      password: 'alice@gmail.com@@##',
+      address: 'kigali',
+      user_type: 'buyer',
+      is_admin: 'false'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('message').eql('your email must look like  this ex:andela@gmail.com');
+      done();
+    });
+  });
+  it('should not register new user ', (done) => {
+    chai.request(server).post('/api/v2/auth/signup').send({
+      email: 'alice@gmail.com',
+      first_name: 'nshuti',
+      last_name: 'jonathan',
+      password: 'alice@gmail.com@@##',
+      address: 'kigali',
+      user_type: 'buyersr',
+      is_admin: 'false'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('message').eql('this user_type field must be seller or buyer');
+      done();
+    });
+  });
+  it('should not register new user ', (done) => {
+    chai.request(server).post('/api/v2/auth/signup').send({
+      email: 'alice@gmail.com',
+      first_name: 'nshuti',
+      last_name: 'jonathan',
+      password: 'alice@gmail.com@@##',
+      address: 'kigali',
+      user_type: 'buyer',
+      is_admin: 'true'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('message').eql('is_admin field must be set to false');
+      done();
+    });
+  });
+});
+describe('Users', () => {
+  it('should sign in user', (done) => {
+    chai.request(server).post('/api/v2/auth/signin').send({
+      email: 'alice@gmail.com',
+      password: 'chris@gmail.com',
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(200);
+      res.body.should.have.property('message').eql('Logged in successfully');
+      done();
+    });
+  });
+  it('should not sign in user', (done) => {
+    chai.request(server).post('/api/v2/auth/signin').send({
+      email: 'alice@gmail.com',
+      password: '2wer'
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(401);
+      res.body.should.have.property('message').eql('INVALID email or password');
+      done();
+    });
+  });
+  it('should not sign in user', (done) => {
+    chai.request(server).post('/api/v2/auth/signin').send({
+      email: '',
+      password: ''
+    }).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(400);
+      res.body.should.have.property('message').eql('Some values are missing');
+      done();
+    });
+  });
+});
+describe('Admin', () => {
+  it('should not  sign in admin ', (done) => {
+    chai.request(server).post('/api/v2/auth/signin/admin').send({
+      email: 'rrur@gmail.com',
+      password: 'ddss',
+
+    })
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+  it('should not  sign in admin ', (done) => {
+    chai.request(server).post('/api/v2/auth/signin/admin').send({
+      email: '',
+      password: '',
+
+    })
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+});
+describe('Users', () => {
+  it('should get all users', (done) => {
+    chai.request(server).get('/api/v2/users').set('x-auth-token', token).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(200);
+      res.body.should.have.property('message').eql('Users retrieved successfully');
+      res.body.data.should.be.an('array');
+      done();
+    });
+  });
+  it('should not get all  users', (done) => {
+    chai.request(server).get('/api/v2/users').end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(402);
+      res.body.should.have.property('message').eql('Access Denied.No token provided');
+      done();
+    });
+  });
+  it('should not get all  users', (done) => {
+    chai.request(server).get('/api/v2/users').set('x-auth-token', fakeToken).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(400);
+      res.body.should.have.property('message').eql('Invalid token');
       done();
     });
   });
 });
 describe('Users', () => {
   it('should delete a user', (done) => {
-    const admin = {
-      is_admin: 'false'
-    };
-    const token = jwt.sign(admin, process.env.jwtPrivateKey);
-    chai.request(server).delete('/api/v2/users/:100000000').set('x-auth-token', token).end((err, res) => {
+    chai.request(server).delete('/api/v2/users/1').set('x-auth-token', token).end((err, res) => {
+      console.log(res.body);
       res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(200);
+      res.body.should.have.property('message').eql('User with id 1 deleted successfully');
+      done();
+    });
+  });
+  it('should not delete a user', (done) => {
+    chai.request(server).delete('/api/v2/users/10').set('x-auth-token', token).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(404);
+      res.body.should.have.property('message').eql('User with id 10 not found');
+      done();
+    });
+  });
+  it('should not delete a user', (done) => {
+    chai.request(server).delete('/api/v2/users/1').end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(402);
+      res.body.should.have.property('message').eql('Access Denied.No token provided');
+      done();
+    });
+  });
+  it('should not delete a user', (done) => {
+    chai.request(server).delete('/api/v2/users/1').set('x-auth-token', fakeToken).end((err, res) => {
+      console.log(res.body);
+      res.body.should.be.an('object');
+      res.body.should.have.property('status').eql(400);
+      res.body.should.have.property('message').eql('Invalid token');
       done();
     });
   });
